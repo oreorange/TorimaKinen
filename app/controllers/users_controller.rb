6b7b788110caf3f4406149149c8d84735db6894b
oreorange <92431652+oreorange@users.noreply.github.com>
2022-01-17 14:28:53 +0000

    [Fix]user_info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   private
