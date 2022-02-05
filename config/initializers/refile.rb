e06a70f310afeb1a40cf0f66f30ab3da535dc3fa
oreorange <92431652+oreorange@users.noreply.github.com>
2022-02-05 08:29:34 +0000

    [Fix]gitignore

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,4 +1,2 @@
 Refile.backends['store'] = Refile::Backend::FileSystem.new('public/uploads/')
 # 画像アップロード先を変更　デフォルトはtmp/uploads/store
-
-Refile.backends['cache'] = Refile::Backend::FileSystem.new('public/cache/')
